62985fcb1f017ed598cd6a6a691053751411083c
Matthew Lee <laughew@gmail.com>
2020-02-10 21:14:14 -0800

    removed unneeded class, added some missing methods, added tests for PhotoEntry methods

--- src/test/model/TagTest.java
+++ src/test/model/TagTest.java
@@ -1,4 +1,21 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TagTest {
+
+    Tag t;
+
+    @BeforeEach
+    void runBefore() {
+        t = new Tag("#selfie");
+    }
+
+    @Test
+    void testGetTag() {
+        assertEquals("#seflie", t.getTag());
+    }
 }
